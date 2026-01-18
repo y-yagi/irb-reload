be97e5aeb5bd308e2d7fdb74f480143d78c3f2b6
Yuji Yaginuma <yuuji.yaginuma@gmail.com>
2026-01-18 14:03:54 +0900

    Use `watchcat`

--- test/irb/reload_test.rb
+++ test/irb/reload_test.rb
@@ -5,62 +5,89 @@ require "set"
 
 module IRB
   class ReloadTest < Minitest::Test
-    def test_start_respects_configured_paths_and_listen_options
+    def test_start_respects_configured_paths_and_watchcat_filters
       IRB::Reload.config[:paths] = %w[app services]
 
-      listener = with_listener_stub do
+      watcher = with_watchcat_stub do
         IRB::Reload.start
       end
 
-      assert_equal %w[app services], listener.paths
+      assert_equal %w[app services], watcher.paths
+      assert_equal({ ignore_remove: true, ignore_access: true }, watcher.options[:filters])
     end
 
     def test_record_updated_and_added_ruby_files_only_once
-      listener = with_listener_stub do
+      watcher = with_watchcat_stub do
         IRB::Reload.start
       end
 
-      listener.simulate_change(modified: ["lib/foo.rb", "lib/foo.rb"], added: ["README.md", "lib/bar.rb"])
+      watcher.simulate_event(paths: ["lib/foo.rb", "lib/foo.rb", "README.md", "lib/bar.rb"], event_type: :modify)
+      watcher.simulate_event(paths: ["lib/baz.rb"], event_type: :create)
 
-      assert_equal Set["lib/foo.rb", "lib/bar.rb"], IRB::Reload.instance_variable_get(:@changed_files)
+      assert_equal Set["lib/foo.rb", "lib/bar.rb", "lib/baz.rb"], IRB::Reload.instance_variable_get(:@changed_files)
     end
 
     private
 
-    def with_listener_stub
-      listener = nil
+    def with_watchcat_stub
+      watcher = nil
 
-      Listen.stub(:to, ->(*paths, **options, &block) { listener = ListenerDouble.new(paths, options, block) }) do
+      Watchcat.stub(:watch, ->(paths, **options, &block) { watcher = WatcherDouble.new(paths, options, block) }) do
         yield
       end
 
-      listener
+      watcher
     end
 
-    class ListenerDouble
+    class WatcherDouble
       attr_reader :paths, :options
 
       def initialize(paths, options, block)
-        @paths = paths
+        @paths = Array(paths)
         @options = options
         @block = block
-        @started = false
       end
 
-      def start
-        @started = true
+      def stop; end
+
+      def simulate_event(paths:, event_type: :modify)
+        event = EventDouble.new(paths, event_type)
+        @block.call(event)
+      end
+    end
+
+    class EventDouble
+      attr_reader :paths, :kind
+
+      def initialize(paths, event_type)
+        @paths = Array(paths)
+        @kind = KindDouble.new(event_type)
+      end
+    end
+
+    class KindDouble
+      def initialize(event_type)
+        @event_type = event_type
       end
 
-      def started?
-        @started
+      def create?
+        @event_type == :create
       end
 
-      def directories
-        paths
+      def modify?
+        @event_type == :modify
       end
 
-      def simulate_change(modified: [], added: [], removed: [])
-        @block.call(modified, added, removed)
+      def any?
+        @event_type == :any
+      end
+
+      def remove?
+        @event_type == :remove
+      end
+
+      def access?
+        @event_type == :access
       end
     end
   end
